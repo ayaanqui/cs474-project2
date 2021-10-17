52602b5fd3be3208d2f583d1c44155bbabc07eab
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-10-17 15:15:36 -0500

    add setup commands

--- main.rb
+++ main.rb
@@ -1,1 +1,25 @@
-puts "Hello world"
+class Project2
+  def initialize
+  end
+
+  def program_loop
+    while true
+      input = gets
+      command = input[0]
+
+      case command
+      when "r"
+        puts "Run the file until the end."
+      when "s"
+        puts "Run one line at a time."
+      when "x"
+        break
+      else
+        puts "Unknown command"
+      end
+    end
+  end
+end
+
+program = Project2.new
+program.program_loop
