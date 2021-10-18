236adfd9d25134b92ab1c93023c741b9549f6a82
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-10-17 19:38:08 -0500

    add method to read and return file data as an array

--- main.rb
+++ main.rb
@@ -2,6 +2,15 @@ class Project2
   def initialize
   end
 
+  # @param filename String
+  # @return Array
+  def get_file_data(filename)
+    file = File.open filename
+    data = file.readlines.map { |line| line.chomp }
+    file.close
+    data
+  end
+
   def program_loop
     while true
       input = gets
@@ -9,7 +18,7 @@ class Project2
 
       case command
       when "r"
-        puts "Run the file until the end."
+        puts(get_file_data "pc_input.txt")
       when "s"
         puts "Run one line at a time."
       when "x"
