de0f644129de54d847141ec5b7ebed4131cf2b10
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-10-17 22:10:59 -0500

    add pc_input.txt file documentation

--- main.rb
+++ main.rb
@@ -2,6 +2,15 @@ class Project2
   def initialize
   end
 
+  # pc_input.txt format style:
+  # The syntax of each arithmetic expression conforms to the following four patterns
+  # where id can be one of: w, x, y, and z.
+  # And op can be one of: +, -, *, **, and /.
+  # 1. id = id op id.
+  # 2. id = id op constant.
+  # 3. id = constant.
+  # 4. id ? go int.
+  #
   # @param filename String
   # @return Array
   def get_file_data(filename)
@@ -20,7 +29,7 @@ class Project2
       when "r"
         puts(get_file_data "pc_input.txt")
       when "s"
-        puts "Run one line at a time."
+ s       puts "Run one line at a time."
       when "x"
         break
       else
