94dae63801bca90983bdda518c0712957b050557
Ayaan Siddiqui <ayaanqui.com@gmail.com>
2021-10-21 18:14:05 -0500

    add variable snapshot class

---
+++ variable_snapshot.rb
@@ -0,0 +1,13 @@
+class VariableSnapshot
+  attr_accessor(:w)
+  attr_accessor(:x)
+  attr_accessor(:y)
+  attr_accessor(:z)
+
+  def initialize
+    @w = 0
+    @x = 0
+    @y = 0
+    @z = 0
+  end
+end
